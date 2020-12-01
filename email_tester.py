33d8335727fed5fc78a5f88e94425af6617b7f66
Ryan Lampe <rlampe@huskers.unl.edu>
2020-12-01 12:07:54 -0600

    added git ignore for credentials file

--- email_tester.py
+++ email_tester.py
@@ -5,8 +5,8 @@ import smtplib
 import ssl
 from email.message import EmailMessage
 
-user_email = 'learningassistants@gmail.com'
-user_password = 'TODO'
+user_email = 'cselearningassistant@gmail.com'
+user_password = 'CSELearningAssistantProgram!'
 
 to = ['glampe99@gmail.com']
 subject = 'Test of Python Mailer'
@@ -22,7 +22,10 @@ text = msg.as_string()
 
 
 #send
+print("Creating SSL Context")
 context = ssl.create_default_context()
+
+print("Logging In")
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = context) as server:
     server.login(user_email, user_password)
     for addr in to:
