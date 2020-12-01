f03a0857a9d05e2d5a6b2b15717d0892a3c107a5
Ryan Lampe <rlampe@huskers.unl.edu>
2020-12-01 12:10:39 -0600

    added credentials template

---
+++ credentials_template.py
@@ -0,0 +1,7 @@
+#credentials_template.py
+# This is a template file for storing config settings
+# so that passwords and logins aren't pushed to git
+# after cloning the repo, update the following fields
+
+user_email = ''
+user_email_password = ''
