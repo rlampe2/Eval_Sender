d5a2ba60a08f06e47e853567327af1c8caebf726
Ryan Lampe <rlampe@huskers.unl.edu>
2020-12-01 11:55:06 -0600

    implmenented student *struct*

---
+++ box_tester.py
@@ -0,0 +1,8 @@
+#Temp Python file to work on downloading files from Box api
+
+
+#How to setup authentication 101
+#https://github.com/box/box-python-sdk/blob/master/docs/usage/authentication.md
+#https://developer.box.com/guides/applications/custom-apps/app-token-setup/
+
+#CANNOT USE BOX API UNTIL ADMIN APPROVE MY TOKEN...
--- eval_sender.py
+++ eval_sender.py
@@ -9,7 +9,26 @@
 # 4. Run the program!
 
 
+
+
+#Misc:
+#
+# Apparently, you need to know the file id to download a file from box, not just the filepath... so search API! 
+# https://github.com/box/box-node-sdk/issues/344
+# https://stackoverflow.com/questions/45870835/how-to-obtain-file-id-from-file-name-in-box-com-api
+
+
 import xlrd
+
+#LA struct
+class LA:
+    def __init__(self, firstName, lastName, email, course, evalFound, downloadSuccess):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.evalFound = evalFound
+        self.course = course
+        self.downloadSuccess = downloadSuccess
 
 #TODO Connect to Box for files
 
@@ -17,17 +36,25 @@ import xlrd
 
 
 #Open file:
-file_path = ("C:\\Users\\rjlam\\Documents\\LAP\\LA_Roster.xlsx")
+file_path = ("C:\\Users\\rjlam\\Documents\\LAP\\Evaluation_Automation\\LA_Roster.xlsx")
 #Open book:
 wb = xlrd.open_workbook(file_path)
 sheet = wb.sheet_by_index(0)
 
 print("Looking for mathing names and evaluation files")
 
-#build a dictionary of names that map to a list (contains firstname, lastname, email, course, download successful etc)
-las = {}
+#build a list of las = []
+las = list()
 
 for i in range(1, sheet.nrows): #skip the first row as it should be column headers
-    las[sheet.cell_value(i,0) + "_" + sheet.cell_value(i,1)] = [sheet.cell_value(i,2)]
+    fname = sheet.cell_value(i, 0)
+    lname = sheet.cell_value(i, 1)
+    email = sheet.cell_value(i, 2)
+    course = sheet.cell_value(i, 3)
+    la = LA(fname, lname, email, course, False, False)
+    las.append(la)
 
-print(las)
+for p in las:
+    prettyName = "%-10s, %-10s --- %15s" % (p.firstName, p.lastName, p.email)
+    print(prettyName)
+#    print(p.firstName + ", " + p.lastName + " --- " + p.email)
